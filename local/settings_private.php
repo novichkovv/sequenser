a1fe9e146c21e20b1a8c3c41e982c04085b19dd5
novichkovv <novichkovv@bk.ru>
2016-06-28 15:36:03 +0300

    No message
    Empty message
    
    Signed-off-by: novichkovv <novichkovv@bk.ru>

--- local/settings_private.php
+++ local/settings_private.php
@@ -4,9 +4,9 @@ return array(
     'k_c' => '1', //CAPTCHA keys
     'k_p' => '2',
     'k_h' => '3',
-	'mysql_server' => 'localhost',
-	'mysql_user' => 'root',
-	'mysql_pass' => '',
-	'mysql_db' => 'sequensor',
+    'mysql_server' => 'localhost',
+    'mysql_user' => 'sequensor',
+    'mysql_pass' => 'asd12345',
+    'mysql_db' => 'sequensor',
 );
 ?>
